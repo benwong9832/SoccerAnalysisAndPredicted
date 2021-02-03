1ee40ba1f6082cac335d5909bfb67673e415f9bb
Benwong <kenpetere@gmail.com>
2021-02-03 17:55:29 +0800

    upload mac

--- dataConvert/jsonCsvMatchLineups2.py
+++ dataConvert/jsonCsvMatchLineups2.py
@@ -18,13 +18,20 @@ leaguesNameDic = dataDictionary.leaguesNameDic
 leaguesNameToId = dataDictionary.leaguesNameToId
 
 rowData = {
-  'match_id': [], 'season': [], 'team': [], 'formation': [], 
-  'player_id': [], 'common_name': [], 'weight': [], 'height': [], 
-  'country': [], 
-  'number': [], 'position': [], 'order': []
+    'match_id': [], 'season': [],
+
+    'HTF': [],
+    'HP1N': [], 'HP1P': [], 'HP2N': [], 'HP2P': [], 'HP3N': [], 'HP3P': [], 'HP4N': [], 'HP4P': [], 'HP5N': [], 'HP5P': [], 'HP6N': [], 'HP6P': [],
+    'HP7N': [], 'HP7P': [], 'HP8N': [], 'HP8P': [], 'HP9N': [], 'HP9P': [], 'HP10N': [], 'HP10P': [], 'HP11N': [], 'HP11P': [],
+
+    'ATF': [],
+    'AP1N': [], 'AP1P': [], 'AP2N': [], 'AP2P': [], 'AP3N': [], 'AP3P': [], 'AP4N': [], 'AP4P': [], 'AP5N': [], 'AP5P': [], 'AP6N': [], 'AP6P': [],
+    'AP7N': [], 'AP7P': [], 'AP8N': [], 'AP8P': [], 'AP9N': [], 'AP9P': [], 'AP10N': [], 'AP10P': [], 'AP11N': [], 'AP11P': [],
 }
 
-data_file = open(os.getcwd() + '\\csvData\\match\\matchLineups.csv', 'w', newline='', encoding='utf-8')
+# data_file = open(os.getcwd() + '\\csvData\\match\\matchLineups2.csv', 'w', newline='', encoding='utf-8')
+data_file = open(os.getcwd() + '/csvData/match/matchLineups2.csv', 'w', newline='', encoding='utf-8')
+print(os.getcwd() + '/csvData/match/matchLineups2.csv')
 csv_writer = csv.writer(data_file)
 
 
@@ -37,7 +44,8 @@ csv_writer = csv.writer(data_file)
 file_paths = []
 for league, seasons in leaguesFileDic.items():
     for season in seasons:
-        path = os.getcwd() + '\\data\\matchLineups\\' + league + '\\' + season
+        # path = os.getcwd() + '\\data\\matchLineups\\' + league + '\\' + season
+        path = os.getcwd() + '/data/matchLineups/' + league + '/' + season
         for f in glob.glob(path + "**/*.json", recursive=True):
             file_paths.append(f)
 
@@ -57,58 +65,37 @@ for file_path in file_paths:
     	data = json.load(json_file)
     
     if 'data' in data:
+        if len(data['home']['squad']) != 11 or len(data['away']['squad']) != 11:
+            print(file_path)
+            break
+
         data = data['data']
 
+        rowData['match_id'].append(match_id)
+        rowData['season'].append(season)
+
+        rowData['HTF'].append(data['home']['formation']) if data['home']['formation'] != None else rowData['HTF'].append('NaN')
+
+        cout = 1        
         for player in data['home']['squad']:
-            rowData['match_id'].append(match_id)
-            rowData['season'].append(season)
-            rowData['team'].append('home')
-            rowData['formation'].append(data['home']['formation']) if data['home']['formation'] != None else rowData['formation'].append('NaN')
+            if cout > 11: 
+                print(file_path)
+                break
+            rowData['HP' + str(cout) +'N'].append(player['number']) if player['number'] != None else rowData['HP' + str(cout) +'N'].append('NaN')
+            rowData['HP' + str(cout) +'P'].append(player['position']) if player['position'] != None else rowData['HP' + str(cout) +'P'].append('NaN')
+            cout = cout + 1
 
-            rowData['player_id'].append(player['player']['id']) if player['player']['id'] != None else rowData['player_id'].append('NaN')
-            rowData['common_name'].append(player['player']['common_name']) if player['player']['common_name'] != None else rowData['common_name'].append('NaN')
-            
-            if player['player']['weight'] != None and player['player']['weight'] != 0 and player['player']['weight'] != '0':
-                rowData['weight'].append(player['player']['weight'])
-            else:
-                rowData['weight'].append('NaN')
-            # rowData['weight'].append(player['player']['weight']) if player['player']['weight'] != None else rowData['weight'].append('NaN')
-            
-            # if player['player']['height'] != None or player['player']['height'] != 0:
-            #     rowData['height'].append(player['player']['height'])
-            # else:
-            #     rowData['height'].append('NaN')
-            rowData['height'].append(player['player']['height']) if player['player']['height'] != None else rowData['height'].append('NaN')
-            rowData['country'].append(player['player']['country']['name']) if player['player']['country']['name'] != None else rowData['country'].append('NaN')
-            
-            rowData['number'].append(player['number']) if player['number'] != None else rowData['number'].append('NaN')
-            rowData['position'].append(player['position']) if player['position'] != None else rowData['position'].append('NaN')
-            rowData['order'].append(player['order']) if player['order'] != None else rowData['order'].append('NaN')
-        
+        rowData['ATF'].append(data['home']['formation']) if data['home']['formation'] != None else rowData['ATF'].append('NaN')
+
+        cout = 1
         for player in data['away']['squad']:
-            rowData['match_id'].append(match_id)
-            rowData['season'].append(season)
-            rowData['team'].append('away')
-            rowData['formation'].append(data['home']['formation']) if data['home']['formation'] != None else rowData['formation'].append('NaN')
-
-            rowData['player_id'].append(player['player']['id']) if player['player']['id'] != None else rowData['player_id'].append('NaN')
-            rowData['common_name'].append(player['player']['common_name']) if player['player']['common_name'] != None else rowData['common_name'].append('NaN')
-            if player['player']['weight'] != None and player['player']['weight'] != 0 and player['player']['weight'] != '0':
-                rowData['weight'].append(player['player']['weight'])
-            else:
-                rowData['weight'].append('NaN')
-            # rowData['weight'].append(player['player']['weight']) if player['player']['weight'] != None else rowData['weight'].append('NaN')
+            if cout > 11: 
+                print(file_path)
+                break
+            rowData['AP' + str(cout) +'N'].append(player['number']) if player['number'] != None else rowData['AP' + str(cout) +'N'].append('NaN')
+            rowData['AP' + str(cout) +'P'].append(player['position']) if player['position'] != None else rowData['AP' + str(cout) +'P'].append('NaN')
+            cout = cout + 1
             
-            # if player['player']['height'] != None or player['player']['height'] != 0:
-            #     rowData['height'].append(player['player']['height'])
-            # else:
-            #     rowData['height'].append('NaN')
-            rowData['height'].append(player['player']['height']) if player['player']['height'] != None else rowData['height'].append('NaN')
-            rowData['country'].append(player['player']['country']['name']) if player['player']['country']['name'] != None else rowData['country'].append('NaN')
-            
-            rowData['number'].append(player['number']) if player['number'] != None else rowData['number'].append('NaN')
-            rowData['position'].append(player['position']) if player['position'] != None else rowData['position'].append('NaN')
-            rowData['order'].append(player['order']) if player['order'] != None else rowData['order'].append('NaN')
 
 # insert header
 row = []
